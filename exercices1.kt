81f631d16168c4f41e5e5639f71408078aaac0e6
Rostant87 <tchoutowandja.rostant@ictuniversity.edu.cm>
2026-03-07 20:48:23 +0100

    Suppression des commentaires inutiles

--- exercices1.kt
+++ exercices1.kt
@@ -1,11 +1,8 @@
-// Exercice 1 : Modéliser un Zoo
-// 1. Classe abstraite avec propriété legs et méthode abstraite
 abstract class Animal(val name: String) {
     abstract val legs: Int
     abstract fun makeSound()
 }
 
-// 2. Subclasses concrètes
 class Dog(name: String) : Animal(name) {
     override val legs = 4
     override fun makeSound() {
@@ -21,10 +18,10 @@ class Cat(name: String) : Animal(name) {
 }
 
 fun main() {
-    // 3. Liste d'animaux et itération (Polymorphisme)
-    val animals = listOf(Dog("Buddy"), Cat("Whiskers"))
+        val animals = listOf(Dog("Buddy"), Cat("Whiskers"))
 
     animals.forEach { animal ->
         animal.makeSound()
     }
+
 }
